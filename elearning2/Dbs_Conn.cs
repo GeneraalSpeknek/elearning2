04e571f98a43cdf7f535a52f3d5b3f02c240f6eb
Jordy <Jordy@JORDY-PC>
2016-11-22 12:58:17 +0100

    Add project files.

---
+++ elearning2/Dbs_Conn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace elearning2
+{
+    class Dbs_Conn
+    {
+        string strcnn = "Server=localhost;Database=elearning;uid=root;pwd=;";
+
+        public DataTable LoginCheck(string usrname, string pwd)
+        {
+            MySqlConnection cnn = new MySqlConnection(strcnn);
+            cnn.Open();
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM inloginfo WHERE usrname = "+usrname+" AND pass ="+pwd);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            DataTable tbl = new DataTable();
+            tbl.Load(rdr);
+            cnn.Close();
+            return tbl;
+        }
+    }
+}
